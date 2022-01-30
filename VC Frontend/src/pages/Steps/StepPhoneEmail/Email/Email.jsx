3322cd3afc58c566283a6be8310408a47a91c7e4
nazreen <nazreenshaikh0077@gmail.com>
2022-01-30 14:53:27 +0530

    authentication page

--- VC Frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
+++ VC Frontend/src/pages/Steps/StepPhoneEmail/Email/Email.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react"
-import Card from "../../../../Components/Shared/Card/Card"
-import InputText from "../../../../Components/Shared/InputText/InputText"
-import styles from "../StepPhoneEmail.module.css"
-import Button from '../../../../Components/Shared/Button/Button';
+import { useState } from 'react'
+import Button from '../../../../Components/Shared/Button/Button'
+import Card from '../../../../Components/Shared/Card/Card'
+import InputText from '../../../../Components/Shared/InputText/InputText'
+import styles from '../StepPhoneEmail.module.css'
 
 const Email = ({ onNext }) => {
     const [email, setEmail] = useState('')
 
     return (
-        <Card title={"Enter Your Email"} icon="email-emoji">
+        <Card title={'Enter Your Email'} icon="email-emoji">
             <InputText
                 type="email"
                 placeholder="Email"
@@ -19,7 +19,10 @@ const Email = ({ onNext }) => {
                 <div className={styles.actionButtonWrapper}>
                     <Button text="Next" onClick={onNext} />
                 </div>
-                <p className={styles.bottomParagraph}>By entering your email, you are agreeing to our Terms of Service and Privacy Policy. Thanks :&#41;</p>
+                <p className={styles.bottomParagraph}>
+                    By entering your email, you are agreeing to our Terms of
+                    Service and Privacy Policy. Thanks :&#41;
+                </p>
             </div>
         </Card>
     )
--- VC Frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ VC Frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -1,29 +1,45 @@
-import { useState } from "react"
-import Card from "../../../../Components/Shared/Card/Card"
-import InputText from "../../../../Components/Shared/InputText/InputText"
-import styles from "../StepPhoneEmail.module.css"
-import Button from "../../../../Components/Shared/Button/Button"
+import { useState } from 'react'
+import Card from '../../../../Components/Shared/Card/Card'
+import InputText from '../../../../Components/Shared/InputText/InputText'
+import styles from '../StepPhoneEmail.module.css'
+import Button from '../../../../Components/Shared/Button/Button'
+import { toast } from 'react-toastify'
+import { sendOtp } from '../../../../http'
+import { useDispatch } from 'react-redux'
+import { setOtp } from '../../../../store/authSlice'
 
 const Phone = ({ onNext }) => {
     const [phoneNumber, setPhoneNumber] = useState('')
+    const dispatch = useDispatch()
 
     const onSubmit = async () => {
-        onNext()
+        if (!phoneNumber) {
+            return toast.error('Phone number is required')
+        }
+        try {
+            const { data } = await sendOtp({ phone: phoneNumber })
+            dispatch(setOtp({ phone: data.phone, hash: data.hash }))
+            onNext()
+        } catch (err) {
+            return toast.error(err.response.data.message)
+        }
     }
-
     return (
-        <Card title={"Enter Your Phone Number"} icon="phone">
+        <Card title={'Enter Your Phone Number'} icon="phone">
             <InputText
                 placeholder="Phone Number"
                 value={phoneNumber}
                 onChange={(e) => setPhoneNumber(e.target.value)}
-                maxLength={13}
+                maxLength={15}
             />
             <div>
                 <div className={styles.actionButtonWrapper}>
                     <Button text="Next" onClick={onSubmit} />
                 </div>
-                <p className={styles.bottomParagraph}>By entering your number, you are agreeing to our Terms of Service and Privacy Policy. Thanks :&#41;</p>
+                <p className={styles.bottomParagraph}>
+                    By entering your number, you are agreeing to our Terms of
+                    Service and Privacy Policy. Thanks :&#41;
+                </p>
             </div>
         </Card>
     )
