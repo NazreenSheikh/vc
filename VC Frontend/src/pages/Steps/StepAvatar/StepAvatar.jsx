ae625ab2322359e10b327170ddcb2e341919d270
nazreen <nazreenshaikh0077@gmail.com>
2022-01-30 14:50:46 +0530

    activate pages

--- VC Frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
+++ VC Frontend/src/pages/Steps/StepAvatar/StepAvatar.jsx
@@ -1,9 +1,61 @@
-const StepAvatar = ({ onNext }) => {
+import { useState } from 'react'
+import Card from '../../../Components/Shared/Card/Card'
+import Button from '../../../Components/Shared/Button/Button'
+import { useDispatch, useSelector } from 'react-redux'
+import styles from './StepAvatar.module.css'
+import { setAvatar } from '../../../store/activateSlice'
+import { activate } from '../../../http'
+import { setAuth } from '../../../store/authSlice'
+import { toast } from 'react-toastify'
+
+const StepAvatar = () => {
+    const { name, avatar } = useSelector((state) => state.activate)
+    const [image, setImage] = useState('/images/monkey-avatar.png')
+    const dispatch = useDispatch()
+
+    const captureImage = (e) => {
+        const file = e.target.files[0]
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onloadend = function () {
+            setImage(reader.result)
+            dispatch(setAvatar(reader.result))
+        }
+    }
+
+    const onSubmit = async () => {
+        try {
+            const { data } = await activate({ name, avatar })
+            if (data.auth) {
+                dispatch(setAuth(data))
+            }
+            console.log(data)
+        } catch (err) {
+            return toast.error(err)
+        }
+    }
+
     return (
-        <>
-            <div>Avatar component</div>
-            <button onClick={onNext}>Next</button>
-        </>
+        <Card title={`Hello ${name}!`} icon="monkey-emoji">
+            <p className={styles.subHeading}>Select your Avatar</p>
+            <div className={styles.avatarWrapper}>
+                <img src={image} alt="avatar" className={styles.avatarImage} />
+            </div>
+            <div>
+                <input
+                    onChange={captureImage}
+                    id="avatarInput"
+                    type="file"
+                    className={styles.avatarInput}
+                />
+                <label htmlFor="avatarInput" className={styles.avatarLabel}>
+                    Choose a different photo
+                </label>
+            </div>
+            <div>
+                <Button text="Let's go" onClick={onSubmit} />
+            </div>
+        </Card>
     )
 }
 
--- VC Frontend/src/pages/Steps/StepName/StepName.jsx
+++ VC Frontend/src/pages/Steps/StepName/StepName.jsx
@@ -1,9 +1,37 @@
+import { useState } from 'react'
+import Card from '../../../Components/Shared/Card/Card'
+import Button from '../../../Components/Shared/Button/Button'
+import InputText from '../../../Components/Shared/InputText/InputText'
+import { useDispatch, useSelector } from 'react-redux'
+import styles from './StepName.module.css'
+import { setName } from '../../../store/activateSlice'
+
 const StepName = ({ onNext }) => {
+    const { name } = useSelector((state) => state.activate)
+    const [fullName, setFullName] = useState(name)
+    const dispatch = useDispatch()
+
+    const onSubmit = () => {
+        if (!fullName) {
+            return
+        }
+        dispatch(setName(fullName))
+        onNext()
+    }
+
     return (
-        <>
-            <div>Name component</div>
-            <button onClick={onNext}>Next</button>
-        </>
+        <Card title={'Enter Your Full Name'} icon="goggle-emoji">
+            <InputText
+                placeholder="Full Name"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                maxLength={20}
+            />
+            <p className={styles.paragraph}>Use your real names only please.</p>
+            <div>
+                <Button text="Next" onClick={onSubmit} />
+            </div>
+        </Card>
     )
 }
 
