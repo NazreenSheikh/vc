cc590ccfda5a50669a28eace19f62adab38a15d4
nazreen <nazreenshaikh0077@gmail.com>
2022-01-30 14:57:53 +0530

    home page

--- VC Frontend/src/pages/Home/Home.jsx
+++ VC Frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import styles from './Home.module.css'
-import { Link, useNavigate } from "react-router-dom";
-import Button from '../../Components/Shared/Button/Button';
-import Card from '../../Components/Shared/Card/Card';
+import { Link, useNavigate } from 'react-router-dom'
+import Button from '../../Components/Shared/Button/Button'
+import Card from '../../Components/Shared/Card/Card'
 
 const Home = () => {
     const navigate = useNavigate()
@@ -11,19 +11,21 @@ const Home = () => {
     }
 
     return (
-        <div className={styles.cardWrapper}>
+        <div className="cardWrapper">
             <Card title="Welcome to VoiceClub" icon="logo">
                 <p className={styles.text}>
                     We’re working hard to get VoiceClub ready for everyone!
                     While we wrap up the finishing youches, we’re adding people
                     gradually to make sure nothing breaks
                 </p>
-                <div >
+                <div>
                     <Button text="Get your username" onClick={StartReg} />
                 </div>
-                <div className={styles.signInWrapper} >
-                    <span className={styles.hasInvite}>Have an invite text?</span>
-                    <Link to='/login' className={styles.signInLink}>
+                <div className={styles.signInWrapper}>
+                    <span className={styles.hasInvite}>
+                        Have an invite text?
+                    </span>
+                    <Link to="/login" className={styles.signInLink}>
                         SignIn
                     </Link>
                 </div>
